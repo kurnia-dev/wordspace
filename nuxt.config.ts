17655dad9fdfef33b536e05a79f170bed94085b0
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-07-09 01:09:19 +0700

    setup primevue nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
     'nuxt-vue3-google-signin',
+    '@primevue/nuxt-module'
   ],
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura
+      }
+    }
+  },
   googleSignIn: {
     clientId: '1045411498203-k275rplpsm8n2snkthdudu5j3j04qnla.apps.googleusercontent.com',
   }
